dacb91b4f209aad88a35bd81205f5b748e58cdb3
Emeline Bonte <bonte5u@etu.univ-lorraine.fr>
2021-10-26 11:58:02 +0200

    feat: game evolution after order

--- src/main/java/fr/poweroff/labyrinthe/model/PacmanGame.java
+++ src/main/java/fr/poweroff/labyrinthe/model/PacmanGame.java
@@ -16,7 +16,8 @@ import java.io.IOException;
  */
 public class PacmanGame implements Game {
 
-	private Coordinate pacmanPosition = new Coordinate(0,0);
+	private final Coordinate pacmanPosition = new Coordinate(0,0);
+	private boolean finish = false;
 
 	/**
 	 * constructeur avec fichier source pour le help
@@ -47,11 +48,44 @@ public class PacmanGame implements Game {
 	/**
 	 * faire evoluer le jeu suite a une commande
 	 * 
-	 * @param commande
+	 * @param commande la commande faite par le joueur
 	 */
 	@Override
 	public void evolve(Cmd commande) {
-		System.out.println("Execute "+commande);
+		//System.out.println("Execute "+commande);
+		//récupération des coordonnes du pacman
+		int x = this.pacmanPosition.getCoorX();
+		int y = this.pacmanPosition.getCoorY();
+		//Modification des coordonnees du pacman ou arret du jeu
+		switch (commande) {
+			case UP:
+				this.pacmanPosition.setCoorY(y - 1);
+				System.out.println("Position Pacman :  "+this.pacmanPosition);
+				break;
+			case DOWN:
+				this.pacmanPosition.setCoorY(y + 1);
+				System.out.println("Position Pacman :  "+this.pacmanPosition);
+				break;
+			case LEFT:
+				this.pacmanPosition.setCoorX(x - 1);
+				System.out.println("Position Pacman :  "+this.pacmanPosition);
+				break;
+			case RIGHT:
+				this.pacmanPosition.setCoorX(x + 1);
+				System.out.println("Position Pacman :  "+this.pacmanPosition);
+				break;
+			case EXIT:
+				this.setFinish(true);
+				break;
+		}
+	}
+
+	/**
+	 * Modifie le jeu (jeu en marche ou à l'arret)
+	 * @param finish l'etat du jeu
+	 */
+	public void setFinish(boolean finish) {
+		this.finish = finish;
 	}
 
 	/**
@@ -59,8 +93,7 @@ public class PacmanGame implements Game {
 	 */
 	@Override
 	public boolean isFinished() {
-		// le jeu n'est jamais fini
-		return false;
+		return this.finish;
 	}
 
 }
