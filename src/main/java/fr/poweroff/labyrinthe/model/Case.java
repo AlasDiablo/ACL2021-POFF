6279c751081a0296de99bc4cf9c9bf08f9f64d61
Sven <svenclementoff@gmail.com>
2021-10-29 19:13:28 +0200

    update: change by review

--- src/main/java/fr/poweroff/labyrinthe/model/Case.java
+++ src/main/java/fr/poweroff/labyrinthe/model/Case.java
@@ -4,7 +4,7 @@ import com.sun.jdi.IntegerType;
 import java.awt.image.BufferedImage;
 
 public class Case {
-    private final String NOTYPE = new String("NONE"); // a case with no type
+    private final String NOTYPE = "NONE"; // a case with no type
     private int coordX,coordY; //Coordonnées of this case
 
     private BufferedImage sprite;
@@ -12,22 +12,22 @@ public class Case {
 
     //------------------------------------------------
     public Case(){
-        coordY = -1;
-        coordX = -1;
-        sprite = null;
-        type = NOTYPE;
+        this.coordY = -1;
+        this.coordX = -1;
+        this.sprite = null;
+        this.type = NOTYPE;
     }
     public Case(BufferedImage sp,int x,int y){
-        sprite = sp;
-        coordX = x;
-        coordY = y;
-        type = NOTYPE;
+        this.sprite = sp;
+        this.coordX = x;
+        this.coordY = y;
+        this.type = NOTYPE;
     }
     public Case(BufferedImage sp,int x,int y,String t){
-        sprite = sp;
-        coordX = x;
-        coordY = y;
-        type = new String(t);
+        this.sprite = sp;
+        this.coordX = x;
+        this.coordY = y;
+        this.type = t;
     }
     //------------------------------------------------
     public BufferedImage getSprite() {
